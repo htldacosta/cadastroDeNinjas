18ee014c3ede692f8bb696a0a99a4176fc1de3d1
hitalu da costa <hitaludcosta@gmail.com>
2025-06-03 22:32:18 -0400

    Criado a controller 22:30/03/2025

--- src/main/java/dev/java10x/cadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -7,12 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 
-		int n1 = 25;
-		int n2 = 35;
-		int res = n1 + n2;
-		System.out.println("teste numero 3232323");
-		System.out.print(res);
 	}
 
 }
